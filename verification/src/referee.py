24a6310797a5586df05c2780aa78d0a526fd9d20
Igor <infunt@gmail.com>
2015-04-28 15:20:44 +0300

    Update referee.py

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -10,3 +10,12 @@ class Referee(RefereeBase):
     ENVIRONMENTS = settings_env.ENVIRONMENTS
 
     DEFAULT_FUNCTION_NAME = "check_line"
+
+
+    @gen.coroutine
+    def post_test(self, test, validator_result, category_name, test_number):
+        yield super().post_test(test, validator_result, category_name, test_number)
+        yield self.editor_client.send_post_test({
+            'test_id': test_number, 
+            'message': "Test passed"
+        })
